fd9be2fc7c2291b0339d3b4942c5df6dfef8bf36
syedFaraz <syedfaraz173@gmail.com>
2024-06-10 23:49:22 +0530

    First Commit

---
+++ components/Projects.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "./ui/hero-parallax";
+import { projects } from "../data/index";
+
+export function Projects() {
+  return <HeroParallax products={projects} />;
+}
---
+++ data/index.ts
@@ -0,0 +1,289 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "platforms", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Actively Engaged in Programming Contests and Exploring New Knowledge",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+/*
+  {
+    title: "FilmFusion",
+    link: "https://filmfusion-isr.vercel.app",
+    thumbnail: "/Film.png",
+  },
+  {
+    title: "Momento",
+    link: "https://momento-isr.vercel.app/",
+    thumbnail: "/Momento.png",
+  },
+  {
+    title: "Portfolio",
+    link: "https://syedisrarahmed.vercel.app/",
+    thumbnail: "/Portfolio.png",
+  },
+  {
+    title: "Music-UI project",
+    link: "https://github.com/syed1israr/Acternity_music.git",
+    thumbnail: "/Music.png",
+  },
+
+*/
+export const projects = [
+  {
+    title: "ConvoConnect",
+    link: "https://convoconnect-isr.vercel.app/",
+    thumbnail: "/ConvoConnect.png",
+  },
+  {
+    title: "FilmFusion",
+    link: "https://filmfusion-isr.vercel.app",
+    thumbnail: "/Film.png",
+  },
+  {
+    title: "Momento",
+    link: "https://momento-isr.vercel.app/",
+    thumbnail: "/Momento.png",
+  },
+  {
+    title: "Music-UI project",
+    link: "https://github.com/syed1israr/Acternity_music.git",
+    thumbnail: "/Music.png",
+  },
+  {
+    title: "ConvoConnect",
+    link: "https://convoconnect-isr.vercel.app/",
+    thumbnail: "/ConvoConnect.png",
+  },
+  {
+    title: "FilmFusion",
+    link: "https://filmfusion-isr.vercel.app",
+    thumbnail: "/Film.png",
+  },
+  {
+    title: "Momento",
+    link: "https://momento-isr.vercel.app/",
+    thumbnail: "/Momento.png",
+  },
+  {
+    title: "Music-UI project",
+    link: "https://github.com/syed1israr/Acternity_music.git",
+    thumbnail: "/Music.png",
+  },
+  {
+    title: "ConvoConnect",
+    link: "https://convoconnect-isr.vercel.app/",
+    thumbnail: "/ConvoConnect.png",
+  },
+  {
+    title: "FilmFusion",
+    link: "https://filmfusion-isr.vercel.app",
+    thumbnail: "/Film.png",
+  },
+  {
+    title: "Momento",
+    link: "https://momento-isr.vercel.app/",
+    thumbnail: "/Momento.png",
+  },
+  {
+    title: "Music-UI project",
+    link: "https://github.com/syed1israr/Acternity_music.git",
+    thumbnail: "/Music.png",
+  },
+  {
+    title: "ConvoConnect",
+    link: "https://convoconnect-isr.vercel.app/",
+    thumbnail: "/ConvoConnect.png",
+  },
+  {
+    title: "FilmFusion",
+    link: "https://filmfusion-isr.vercel.app",
+    thumbnail: "/Film.png",
+  },
+  {
+    title: "Momento",
+    link: "https://momento-isr.vercel.app/",
+    thumbnail: "/Momento.png",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+  
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Community Support",
+    desc: "Designed and contributed to projects created by the college community",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Competitive Programming",
+    desc: "Participated in numerous on-campus and online DSA coding rounds",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Full Stack Developer",
+    desc: "Developed and maintained  scalable Web Apps using modern frontend and backend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/syed1israr",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://twitter.com/Syed_Israr_1",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/syed-israr/",
+  },
+];
